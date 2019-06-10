e15d4731e67d1b427af0aa9b834a472d0202f9b9
Alex Martin <amartin@excilys.com>
2019-06-10 16:20:08 +0200

    [Step 2] Display a Recipe

---
+++ prep-formation-react/src/RecipeDetails.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Card, CardBody, CardImg, CardText, CardTitle, Col, Row } from 'reactstrap';
+
+class RecipeDetails extends Component {
+
+    state = {
+        recipe: this.props.recipe
+    };
+
+    render() {
+
+        let { recipe } = this.state;
+
+        return (
+            <div className="recipe-details">
+                <Row>
+                    <Col md={ { size: 6, offset: 3 } }>
+                            <Card>
+                                <CardImg top width="100%" src={ recipe.picture } alt="Card image cap"/>
+                                <CardBody>
+                                    <CardTitle>{ recipe.name }</CardTitle>
+                                    <CardText>{ recipe.description }</CardText>
+                                </CardBody>
+                            </Card>
+                    </Col>
+                </Row>
+            </div>
+        )
+    }
+}
+
+export default RecipeDetails;
